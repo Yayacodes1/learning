65e9ef122dd4a70376124fdf520c0552afb2af92
Yayacodes1 <yo03451122@gmail.com>
2025-12-18 10:10:15 -0600

    finished first execrcise

--- backend/execrsies/9-11/convert.js
+++ backend/execrsies/9-11/convert.js
@@ -1,38 +1,20 @@
 // Callback-based code (convert this to async/await)
-const fs = require('fs');
+const fs = require('fs').promises;
 async  function readAndWrite () {
 
 try{
    const read = await fs.readFile('data.txt', 'utf8');
-   const write= await fs.writeFile('output.txt', data.toUpperCase());
-   console.log('File content:', data);
+   const write= await fs.writeFile('output.txt', read.toUpperCase());
+   console.log('File content:', read);
    console.log('File written successfully!');
    return;
-}catch{
-    if(err){
+}catch(err){
+   
         console.error('Error :', err);
             return;
     }
 }
-}
+
    
    readAndWrite();
     
-// Callback-based code (convert this to async/await)
-const fs = require('fs').promises;
-
-fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error:', err);
-        return;
-    }
-    console.log('File content:', data);
-    
-    fs.writeFile('output.txt', data.toUpperCase(), (err) => {
-        if (err) {
-            console.error('Error writing:', err);
-            return;
-        }
-        console.log('File written successfully!');
-    });
-});
